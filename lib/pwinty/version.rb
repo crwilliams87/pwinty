34883a042166570da9f484038cfbc58e7bb727f9
crwilliams87 <chris.r.williams@icloud.com>
2014-07-14 14:43:28 +0100

    added Api class methods

--- lib/pwinty/version.rb
+++ lib/pwinty/version.rb
@@ -1,15 +1,68 @@
 require "httparty"
 module Pwinty
   VERSION = "0.0.1.alpha"
+  class Api
+    # Sandbox URL
+    ROOT_URL = 'https://sandbox.pwinty.com'
 
-  # Sandbox URL
-  ROOT_URL = 'https://sandbox.pwinty.com'
+    # Live URL
+    # ROOT_URL = 'https://api.pwinty.com'
 
-  # Live URL
-  # ROOT_URL = 'https://api.pwinty.com'
+    def initialize
+      @merchantId = ENV['PWINTY_MERCHANT_ID']
+      @apiKey = ENV['PWINTY_API_KEY']
+    end
 
-  def initialize merchant_id, api_key
-    @merchantId = merchant_id
-    @apiKey = api_key
+    def get_orders
+      request(:get,'/v2.1/Orders',{})
+    end
+
+    def get_order id
+      request(:get,'/v2.1/Orders', { 'query' => { 'id' => id } })
+    end
+
+    def new_order roll, current_user
+      # get an order id
+      order = request(:post,'/Orders',{params:{
+        countryCode: "GB",
+        qualityLevel: "Pro",
+        useTracked: true,
+        recipientName: "#{current_user.name}",
+        payment:"InvoiceRecipient"
+      }})
+
+      order_id = order["id"]
+      order_payment_url = order["paymentUrl"]
+
+      # add roll to pwinty
+      roll.photos.each do |photo|
+        request(:post, "/v2.1/Orders/#{order_id}/Photos", {body:{
+          type: "4x4",
+          url: "#{photo.image}",
+          copies: "1",
+          priceToUser: "99",
+          sizing: "Crop"
+        }})
+      end
+
+      order_valid = request(:get, "/v2.1/Orders/#{order_id}/SubmissionStatus", {})["isValid"]
+
+      if order_valid
+        request(:post, "/v2.1/Orders/#{order_id}/Status", {body:{
+          status: "AwaitingPayment"
+        }})
+        order_payment_url
+      else
+        "error"
+      end
+    end
+
+    def request(action,path,options)
+      headers = { 'X-Pwinty-MerchantId'=> @merchantId,
+                  'X-Pwinty-REST-API-Key' => @apiKey }
+      headers.merge!(options[:headers]) if options[:headers]
+      uri = [ROOT_URL, path].join
+      HTTParty.send(action, uri, headers: headers, query: options[:params], body: options[:body])
+    end
   end
 end
--- spec/pwinty_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe Pwinty do
-  describe '.initialize' do
-    it 'initializes the enviroment variables'
-      Pwinty.initialize('api_key').should eq('api_key')
-    end
-  end
-end
